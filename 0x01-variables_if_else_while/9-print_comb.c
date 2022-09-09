0bfc9af9b28133887531b6d123c1c8998e32410e
Blackburn0 <hamzatadebayo5@gmail.com>
2022-09-09 15:21:32 -0700

    first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description:
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10;  num++)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
