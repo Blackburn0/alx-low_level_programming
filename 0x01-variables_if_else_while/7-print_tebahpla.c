0bce5cbe2635eca982a6a5a263586683436014ec
Blackburn0 <hamzatadebayo5@gmail.com>
2022-09-09 14:58:03 -0700

    first commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,24 @@
-rr
+#include <stdio.h>
+
+/**
+ *main - program execution begins here
+ *
+ *Description: This program prints out all the letters of the alphabet.
+ *Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	char letter;
+
+	letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+
+	return (0);
+}
