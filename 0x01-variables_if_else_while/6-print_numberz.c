0c46f0ac9927c81cb5a73dadf0eb1a4342d7ca52
Blackburn0 <hamzatadebayo5@gmail.com>
2022-09-09 09:10:16 -0700

    first commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,15 +11,10 @@ nclude <stdio.h>
  */
 int main(void)
 {
-	char letter;
+	int d;
 
-	letter = 'z';
-
-	while (letter >= 'a')
-	{
-		putchar(letter);
-		letter--;
-	}
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
 	putchar('\n');
 
 	return (0);
