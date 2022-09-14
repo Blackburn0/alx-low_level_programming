7559cc8e49c83e57ae9abb21ecd546d0f0d7cb34
Blackburn0 <hamzatadebayo5@gmail.com>
2022-09-13 20:29:49 -0700

    first commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,8 @@ void times_table(void)
 			{
 				int num1;
 				int num2;
-																				num1 = temp / 10;														num2 = temp % 10;														_putchar('0' + num1);
+				
+				num1 = temp / 10;														num2 = temp % 10;														_putchar('0' + num1);
 				_putchar('0' + num2);
 			}
 			else
