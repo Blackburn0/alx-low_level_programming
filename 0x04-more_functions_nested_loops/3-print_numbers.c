ea68571a71ece4ef75ec6836cc1e8ae1174948ff
Blackburn0 <hamzatadebayo5@gmail.com>
2022-09-15 11:21:31 -0700

    first commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9,
+ * print_numbers - function that print the number, from 0 to 9,
  * followed by a new line.
  * Return: Always 0
  */
